f211701c6fec0be9de425c90d635b4d1946abb5d
Gregory Farias <gregory@drcuco.com.br>
2022-11-12 16:45:00 -0300

    removing docker

--- src/routes/user.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Http\Controllers\UserController;
-
-Route::post('/user/create', [UserController::class, 'create']);
-Route::post('/user/show', [UserController::class, 'show']);
-Route::get('/user/list', [UserController::class, 'list']);
-Route::post('/user/update', [UserController::class, 'update']);
-Route::post('/user/delete', [UserController::class, 'delete']);
