e9998e3ec358ed94527918fd049beb6eed775d3f
Gregory Farias <gregory@drcuco.com.br>
2022-11-23 22:12:13 -0300

    coisas

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\MemberCaregiverController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return json_encode(['message' => "Welcome to Cuidamos API. Access the URI /api/documentation to see the documentation."]);
 });
